84fac15d8dd53376214a96abcb9412d84aa68195
Wan Lam <wlam42@gatech.edu>
2023-12-16 19:11:20 +0800

    fix app entry file

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,16 @@ import todoRoutes from "./routes";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { Config } from "./config";
-import { Pool } from "pg";
+import pool from "./database";
 import { HttpError } from "http-errors";
 import { Request, Response, NextFunction } from "express";
 
-const app = express();
+export const app = express();
 const port = Config.PORT || 4000;
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(todoRoutes);
+app.use("/api", cors(), todoRoutes);
 app.use(
   (err: HttpError, _req: Request, res: Response, _next: NextFunction): void => {
     res.status(err.status || 500);
@@ -29,12 +28,6 @@ app.use(
   }
 );
 
-const pool = new Pool({
-  connectionString: Config.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
 
 pool.connect((err) => {
   if (err) {
@@ -42,6 +35,6 @@ pool.connect((err) => {
     process.exit(1); // Exit the process if the database connection fails
   }
   app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at port ${port}`);
   });
 });
